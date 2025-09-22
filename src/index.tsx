984032b84ce35b254eb3aa3a75e849ba648e10cf
Eugene July <dzhulay.evgeniy@gmail.com>
2025-09-22 17:15:14 +0300

    feat: add MainScreen and PlaceCard components in ./pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MainScreen from './pages/main-screen/main-screen';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -7,6 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <MainScreen />
   </React.StrictMode>
 );
