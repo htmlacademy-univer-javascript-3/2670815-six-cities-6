b6cb09a1b27fc67c1c0f59a5561d31fb32a0cc38
Eugene July <dzhulay.evgeniy@gmail.com>
2025-09-22 17:19:03 +0300

    feat: add MainScreen and App component for rendering MainScreen in ./pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MainScreen from './pages/main-screen/main-screen';
+import App from './pages/app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <MainScreen />
+    <App />
   </React.StrictMode>
 );
---
+++ src/pages/app/app.tsx
@@ -0,0 +1,6 @@
+import MainScreen from '../main-screen/main-screen';
+
+// App – корневой компонент приложения. Пока рендерит только главную страницу.
+export default function App(): JSX.Element {
+  return <MainScreen />;
+}
