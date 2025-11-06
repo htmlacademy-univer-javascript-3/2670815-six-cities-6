ee410aa071f6dbb7df7411dd75b80cc270143728
Eugene July <62775429+watsmanameha@users.noreply.github.com>
2025-11-06 03:06:15 +0300

    feat(store): initialize redux store and wrap app with provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/app/app';
 import { OFFERS } from './mocks/offers';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
-const OFFERS_COUNT = OFFERS.length; // данные для главной страницы (from mocks)
+const OFFERS_COUNT = OFFERS.length;
 
 root.render(
   <React.StrictMode>
-    <App offersCount={OFFERS_COUNT} offers={OFFERS} />
-  </React.StrictMode>,
+    <App
+      store={store}
+      offersCount={OFFERS_COUNT}
+      offers={OFFERS}
+    />
+  </React.StrictMode>
 );
